2ce06a446eda913b63ebe36b4ba7c01c0b78c691
Arnon Suksiri <arnon.suksili@gmail.com>
2020-10-04 15:03:50 +0700

    PUT via web

--- src/main/java/th/ac/ku/atm/model/BankAccount.java
+++ src/main/java/th/ac/ku/atm/model/BankAccount.java
@@ -48,4 +48,14 @@ public class BankAccount {
     public void setBalance(double balance) {
         this.balance = balance;
     }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "id=" + id +
+                ", customerId=" + customerId +
+                ", type='" + type + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
 }
